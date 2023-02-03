8986c9642116b8a65ca4bfa97d43403259486b2b
Bhavana Shrotri <Bhavana.Shrotri@Klingelnberg.com>
2023-02-03 10:49:56 +0530

    work in progress

---
+++ DynamicAuthorization/PermissionsDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace DynamicAuthorization
+{
+    public class PermissionsDatabase
+    {
+        private readonly string _dbPath;
+        private JsonSerializerOptions serializerOptions = new JsonSerializerOptions() { WriteIndented = true };
+        public PermissionsDatabase(IWebHostEnvironment env)
+        {
+            _dbPath = Path.Combine(env.ContentRootPath, "permissions.json");
+        }
+
+        private Dictionary<string, HashSet<string>> Record => 
+            File.Exists(_dbPath) ?
+            JsonSerializer.Deserialize<Dictionary<string, HashSet<string>>>(File.ReadAllText(_dbPath)) :
+            new();
+
+        public bool HasPermissions(string userId, string permission)
+        {
+            var db = Record;
+            return db.ContainsKey(userId) && db[userId].Contains(permission);
+        }
+
+        public void AddPermissions(string userId, string permission)
+        {
+            var db = Record;
+            if (db[userId] == null && !db[userId].Contains(permission)) return;
+
+            db[userId].Add(permission);
+            File.WriteAllText(_dbPath, JsonSerializer.Serialize(db, serializerOptions));
+        }
+
+        public void RemovePermissions(string userId, string permission)
+        {
+            var db = Record;
+            if (db[userId] == null && !db[userId].Contains(permission)) return;
+
+            db[userId].Remove(permission);
+            File.WriteAllText(_dbPath, JsonSerializer.Serialize(db, serializerOptions));
+        }
+    }
+}
--- DynamicAuthorization/Program.cs
+++ DynamicAuthorization/Program.cs
@@ -1,25 +1,66 @@
+using DynamicAuthorization;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var permissionsList = new PermissionsList();
+
+builder.Services.AddSingleton(permissionsList);
+builder.Services.AddSingleton<PermissionsDatabase>();
+builder.Services.AddAuthentication("cookie").AddCookie("cookie");
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.MapGet("/", (ClaimsPrincipal user) => user.Claims.Select(x => new { x.Type, x.Value }));
+
+app.MapGet("/login", () => Results.SignIn(
+    new ClaimsPrincipal(
+        new ClaimsIdentity(
+            new Claim[] 
+            {
+                new Claim (ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                new Claim (ClaimTypes.Country,"IN"),
+            },
+            "cookie"
+        )),
+    authenticationScheme: "cookie"
+));
+
+
+app.MapGet("/secret/one", ()=>"one").RequireAuthorization().WithTags("auth:secret/one", "auth:secret");
+app.MapGet("/secret/one", ()=>"two").RequireAuthorization().WithTags("auth:secret/two", "auth:secret");
+app.MapGet("/secret/one", ()=>"three").RequireAuthorization().WithTags("auth:secret/three", "auth:secret");
+
+app.MapGet("/permissions", (PermissionsList p)=> p);
+
+app.MapGet("/promote", (
+    string permission,
+    ClaimsPrincipal user,
+    PermissionsDatabase database) =>
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+    database.AddPermissions(userId, permission);
+});
+
+var endpoints = app as IEndpointRouteBuilder;
+var source = endpoints.DataSources.First();
+foreach (var sourceEndpoint in source.Endpoints)
+{
+    var authTags = sourceEndpoint.Metadata.OfType<TagsAttribute>().SelectMany(x => x.Tags).Where(x => x.StartsWith("auth"));
+
+    foreach (var authTag in authTags)
+    {
+        permissionsList.Add(authTag);
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
+
+public class PermissionsList : HashSet<string>
+{
+
+}
